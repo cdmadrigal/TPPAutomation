4977a247c2076ea5d71c883b92a593d66ee12946
cdmadrigal <cristobal.madrigal@gmail.com>
2020-01-15 09:54:09 -0800

    update add-ADDS
    updated to force rename computer and restart

--- powershell-scripts/add-ADDSPrereqs.ps1
+++ powershell-scripts/add-ADDSPrereqs.ps1
@@ -1,7 +1,13 @@
-﻿#install roles and features
+﻿#install roles and features and rename computer
+
+$newname = "tpp"
+Rename-Computer -NewName $newname -force
+
 $featureLogPath = "c:\poshlog\featurelog.txt"
 New-Item $featureLogPath -ItemType file -Force
 $addsTools = "RSAT-AD-Tools"
 
 Add-WindowsFeature $addsTools -LogPath $featureLogPath
 Get-WindowsFeature | Where installed >>$featureLogPath
+
+Restart-Computer
