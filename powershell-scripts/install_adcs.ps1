489026870d184aec81cf52ebb72d594501ffb537
cdmadrigal <cristobal.madrigal@gmail.com>
2020-02-07 09:33:47 -0800

    Update ADCS script
    Updated script to use SHA256

--- powershell-scripts/install_adcs.ps1
+++ powershell-scripts/install_adcs.ps1
@@ -1,8 +1,8 @@
 Install-AdcsCertificationAuthority `
--CAType EnterpriseRootCa `
+-CAType EnterpriseRootCA `
 -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" `
 -KeyLength 2048 `
--HashAlgorithmName SHA1 `
+-HashAlgorithmName SHA256 `
 -ValidityPeriod Years `
 -ValidityPeriodUnits 3 `
 -Force
